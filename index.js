8d26e9bf244577879534be6d455844e4553cf728
Adam Phillips <aphillips@scholastic.co.uk>
2015-11-06 19:59:34 +0000

    Randomise order of entries

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 import makeStore from './src/store';
 import startServer from './src/server';
+import shuffle from './src/shuffle';
 
 export const store = makeStore();
 startServer(store);
 
+let entries = require('./entries.json');
+let shuffledEntries = shuffle(entries);
+
 store.dispatch({
   type: 'SET_ENTRIES',
-  entries: require('./entries.json')
+  entries: shuffledEntries
 });
 store.dispatch({type: 'NEXT'});
